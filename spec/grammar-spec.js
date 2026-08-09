48dace2dd47c74cdd3dd18a7d5b87156649e2bfe
Sebastian Balcerowiak <asiloisad.93@gmail.com>
2026-08-09 18:54:15 +0200

    Complete the Lumine ecosystem rebrand

--- spec/grammar-spec.js
+++ spec/grammar-spec.js
@@ -1,16 +1,16 @@
 describe("Language C# package", function () {
-  beforeEach(() => waitsForPromise(() => atom.packages.activatePackage("language-csharp")));
+  beforeEach(() => waitsForPromise(() => lumine.packages.activatePackage("language-csharp")));
 
   describe("C# Script grammar", () =>
     it("parses the grammar", function () {
-      const grammar = atom.grammars.grammarForScopeName("source.csx");
+      const grammar = lumine.grammars.grammarForScopeName("source.csx");
       expect(grammar).toBeDefined();
       expect(grammar.scopeName).toBe("source.csx");
     }));
 
   describe("C# Cake grammar", () =>
     it("parses the grammar", function () {
-      const grammar = atom.grammars.grammarForScopeName("source.cake");
+      const grammar = lumine.grammars.grammarForScopeName("source.cake");
       expect(grammar).toBeDefined();
       expect(grammar.scopeName).toBe("source.cake");
     }));
--- spec/tree-sitter-grammar-spec.js
+++ spec/tree-sitter-grammar-spec.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 describe("WASM Tree-sitter JavaScript grammar", () => {
   beforeEach(async () => {
-    await atom.packages.activatePackage("language-csharp");
+    await lumine.packages.activatePackage("language-csharp");
   });
 
   it("passes grammar tests", async () => {
